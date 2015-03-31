3ea5eac369c835cdb40e02d51f4a536bbc5f7fa6
novicaa <novicamiketic@gmail.com>
2015-03-31 14:00:38 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="28051 S. Tamiami Trail,Bonita Springs, FL 34135 1-866-MEX-EATS"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone="18666393287" ;// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
